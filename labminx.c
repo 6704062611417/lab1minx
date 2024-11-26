e02d036cfd4787347332125ec7f6eaad7aa58a3d
6704062611417 <s6704062611417@email.kmutnb.ac.th>
2024-11-26 14:56:18 +0700

    ข้อ 1

---
+++ labminx.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 6;
+    
+    if(b>a){
+        printf("b greater than a");
+    }else if(a>b){
+        printf("a greater than b");
+    }
+
+    return 0;
+}
